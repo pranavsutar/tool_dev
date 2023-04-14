248dce0c7e9c24261619f894bf3872defd022bbe
Harsh-45 <90945108+Harsh-45@users.noreply.github.com>
2023-04-14 16:13:29 +0530

    Added CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import MainPage from './components/DefaultPage/Mainpage';
-import NavigationBar from './components/Navbar/Navigationbar';
-import "./App.css"
-import { RegExForm } from './components/RegularExpression/form';
-
+import React, { useState } from "react";
+import axios from "axios";
+import MainPage from "./components/DefaultPage/Mainpage";
+import NavigationBar from "./components/Navbar/Navigationbar";
+import "./App.css";
+import { RegExForm } from "./components/RegularExpression/form";
 
 // import './App.css';
 
 function App() {
-  
-  
   return (
-    <div className='total-main'>
-      <NavigationBar/>
-      <MainPage/>
-      <RegExForm/>
+    <div className="total-main">
+      <NavigationBar />
+      <MainPage />
+      {/* <RegExForm/> */}
     </div>
   );
 }
-
-
-
 
 export default App;
